7305d85ba1ec111fd5d7ff9376691f71d5f3f43e
uttamsengar <uttamsengar28@gmail.com>
2026-09-11 23:51:47 +0530

    First Commit

---
+++ server/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import multer from "multer";
+import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import employeesRouter from "./routes/employeeRoutes.js";
+import profileRouter from "./routes/profileRoutes.js";
+import attendanceRouter from "./routes/attendanceRoutes.js";
+import leaveRouter from "./routes/leaveRoutes.js";
+import payslipRouter from "./routes/payslipsRoutes.js";
+import dashboardRouter from "./routes/dashboardRoutes.js";
+
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
+const app = express()
+const PORT = process.env.PORT || 4000;
+
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+app.use(multer().none())
+
+// Routes
+app.get("/", (req, res)=> res.send("Server is running"));
+app.use("/api/auth", authRouter)
+app.use("/api/employees", employeesRouter)
+app.use("/api/profile", profileRouter)
+app.use("/api/attendance", attendanceRouter)
+app.use("/api/leave", leaveRouter)
+app.use("/api/payslips", payslipRouter)
+app.use("/api/dashboard", dashboardRouter)
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+await connectDB()
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
